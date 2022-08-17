f29c0aaa84065d4032ca12919116fe3f16d0b39a
David Leangen <david@leangen.net>
2022-08-17 20:58:09 +0900

    Add AngularFire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'supertokens-demo-20220805',
+    appId: '1:357461094363:web:6a40acca728fb5f58801fe',
+    storageBucket: 'supertokens-demo-20220805.appspot.com',
+    locationId: 'asia-northeast1',
+    apiKey: 'AIzaSyAQOYDj_1xqPQB7uiHnD9esHNZPjjpM_OA',
+    authDomain: 'supertokens-demo-20220805.firebaseapp.com',
+    messagingSenderId: '357461094363',
+  },
   production: false
 };
 
