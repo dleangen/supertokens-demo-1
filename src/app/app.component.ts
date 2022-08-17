d99cd1b5b69b59b50fa2b5db841feaab4fca866e
David Leangen <david@leangen.net>
2022-08-17 21:20:31 +0900

    Use Angular 14 standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {RouterModule} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
 })
 export class AppComponent {
   title = 'supertokens-demo-1';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import {enableProdMode, importProvidersFrom} from '@angular/core';
 import { environment } from './environments/environment';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {getAuth, provideAuth} from "@angular/fire/auth";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import {getFunctions, provideFunctions} from "@angular/fire/functions";
+import {FIREBASE_OPTIONS} from "@angular/fire/compat";
+import {RouterModule} from "@angular/router";
+import {APP_ROUTES} from "./app/app.routes";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(APP_ROUTES), BrowserAnimationsModule, HttpClientModule),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideFunctions(() => getFunctions()),
+    ),
+  ],
+})
